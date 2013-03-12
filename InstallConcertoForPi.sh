78c704b844e5c10556e44adcc2b600946825d991
Andrew Fryer <flamewave000@live.com>
2013-03-12 09:31:37 -0700

    Made the shoutcast install optional

--- InstallConcertoForPi.sh
+++ InstallConcertoForPi.sh
@@ -38,14 +38,7 @@ done
 
 #This command will install the XSettings and the Unclutter programs
 echo "INSTALLING REQUIRED PROGRAMS"
-sudo apt-get -y --force-yes install x11-xserver-utils unclutter alsa-utils
-res=`sudo cat /etc/modules | grep snd_bcm2835`
-if [ "$res" == "" ]; then
-	sudo echo "snd_bcm2835" >> /etc/modules
-fi
-sudo apt-get install -y --force-yes install mpd mpc
-sudo /etc/init.d/mpd stop
-sudo /bin/chmod -x /etc/init.d/mpd
+sudo apt-get -y --force-yes install x11-xserver-utils unclutter
 
 #This copies the Concerto script to the home directory
 echo "COPYING CRONJOB SCRIPT TO $HOME"
@@ -76,49 +69,76 @@ do
     done
 done
 echo
-#get the ShoutCast Radio server web address, wether it be an IP or URL
-gettingip=true
-while $gettingip;
+while true;
 do
-    echo "Please enter the ShoutCast Radio [URL/IP][:port] address"
-    echo "(http://radio.example.com:8000/ OR http://123.4.5.67:8000/)"
-	echo -n " >> "
-    read radio
-    while true;
-    do
-        echo "You entered: $radio"
-        echo -n "Is this correct [y/n]?"
-        read x
-        case $x in
-          y|Y)
-            gettingip=false
-            break
-          ;;
-          n|N)
-            break
-          ;;
-          *)
-            echo "Invalid command '$x'"
-          ;;
-        esac
-    done
+    echo "Do you wish to setup a ShoutCast Playback?"
+    echo -n "[y/n]? "
+    read x
+    case $x in
+      y|Y)
+			sudo apt-get -y --force-yes install alsa-utils
+			res=`sudo cat /etc/modules | grep snd_bcm2835`
+			if [ "$res" == "" ]; then
+				sudo echo "snd_bcm2835" >> /etc/modules
+			fi
+			sudo apt-get install -y --force-yes install mpd mpc
+			sudo /etc/init.d/mpd stop
+			sudo /bin/chmod -x /etc/init.d/mpd
+			shoutcast=1
+			#get the ShoutCast Radio server web address, wether it be an IP or URL
+			gettingip=true
+			while $gettingip;
+			do
+				echo "Please enter the ShoutCast Radio [URL/IP][:port] address"
+				echo "(http://radio.example.com:8000/ OR http://123.4.5.67:8000/)"
+				echo -n " >> "
+				read radio
+				while true;
+				do
+					echo "You entered: $radio"
+					echo -n "Is this correct [y/n]?"
+					read x
+					case $x in
+					  y|Y)
+						gettingip=false
+						break
+					  ;;
+					  n|N)
+						break
+					  ;;
+					  *)
+						echo "Invalid command '$x'"
+					  ;;
+					esac
+				done
+			done
+			echo
+			#Copy the radio script file header then inject the user's given ShoutCast radio server web address and then append the rest of the script
+			head -n 2 ./.shoutcast > $HOME/shoutcast-radio.sh
+			echo 'shoutcast_radio_link="'$radio'"' >> $HOME/shoutcast-radio.sh
+			tail --lines=+3 ./.shoutcast >> $HOME/shoutcast-radio.sh
+      ;;
+      n|N)
+        echo OK.
+        break
+      ;;
+      *)
+        echo "Invalid command '$x'"
+      ;;
+    esac
 done
-echo
 #Copy the concerto script file header then inject the user's given Concerto server web address and then append the rest of the script
 head -n 4 ./.script > $HOME/digitalsignage.sh
 echo 'ConcertoServerIP="'$ip'"' >> $HOME/digitalsignage.sh
 tail --lines=+5 ./.script >> $HOME/digitalsignage.sh
 
-#Copy the radio script file header then inject the user's given ShoutCast radio server web address and then append the rest of the script
-head -n 2 ./.shoutcast > $HOME/shoutcast-radio.sh
-echo 'shoutcast_radio_link="'$radio'"' >> $HOME/shoutcast-radio.sh
-tail --lines=+3 ./.shoutcast >> $HOME/shoutcast-radio.sh
-
 #This sets up a Cron job (Cron is a task scheduler) that will run the previous Concerto script every minute
 echo "SETTING UP CONCERTO CRON JOB"
 cp ./.crontab ./.tmp
 echo "* * * * * export DISPLAY=:0 && /bin/bash $HOME/digitalsignage.sh" >> ./.tmp
-echo "@reboot   /bin/bash $HOME/shoutcast-radio.sh &> $HOME/shoutcast-radio.log" >> ./.tmp
+if [ $shoutcast ]; then
+	echo "@reboot   /bin/bash $HOME/shoutcast-radio.sh &> $HOME/shoutcast-radio.log" >> ./.tmp
+fi
 crontab -u $USER ./.tmp
 rm ./.tmp
 
